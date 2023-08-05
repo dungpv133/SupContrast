dbb3db6c4e7415d9fd8b261d5f8c99a21be21f53
Viet Dung <haconga1903@gmail.com>
2023-08-05 12:23:28 +0700

    Update vit_model.py

--- vit_model.py
+++ vit_model.py
@@ -669,7 +669,7 @@ class VisionTransformer(nn.Module):
         # else:
         # print(x.shape)
         x = self.se_block(x)
-        if self.use_head is False:
+        if self.use_head == False:
             return x
 
         x1 = self.head(x)
