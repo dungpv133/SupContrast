cf1343fe90ea082ec49d7ea861debf72cab44da7
Viet Dung <haconga1903@gmail.com>
2023-08-06 10:03:31 +0700

    Update main_supcon.py

--- main_supcon.py
+++ main_supcon.py
@@ -187,9 +187,9 @@ def set_model(opt):
         model = apex.parallel.convert_syncbn_model(model)
 
     if torch.cuda.is_available():
-        if torch.cuda.device_count() > 1:
-            model.encoder = torch.nn.DataParallel(model.encoder)
-        model = model.cuda()
+        # if torch.cuda.device_count() > 1:
+        #     model.encoder = torch.nn.DataParallel(model.encoder)
+        model = torch.nn.DataParallel(model).cuda()
         criterion = criterion.cuda()
         cudnn.benchmark = True
 
