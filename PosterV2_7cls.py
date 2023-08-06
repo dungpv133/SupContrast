a6833666aabce1fd75b108c07388df68ff55d78a
Viet Dung <haconga1903@gmail.com>
2023-08-06 09:49:50 +0700

    Update PosterV2_7cls.py

--- PosterV2_7cls.py
+++ PosterV2_7cls.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from .mobilefacenet import MobileFaceNet
-from .ir50 import Backbone
-from .vit_model import VisionTransformer, PatchEmbed
+from mobilefacenet import MobileFaceNet
+from ir50 import Backbone
+from vit_model import VisionTransformer, PatchEmbed
 from timm.models.layers import trunc_normal_, DropPath
 from thop import profile
 
