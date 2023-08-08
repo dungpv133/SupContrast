c8cddd0d370db53773a6dd253e40be76b9cfa81a
Viet Dung <haconga1903@gmail.com>
2023-08-08 11:59:47 +0700

    Update main_supcon_original.py

--- main_supcon_original.py
+++ main_supcon_original.py
@@ -219,8 +219,8 @@ def train(train_loader, model, criterion, optimizer, epoch, opt):
         features = model(images)
         f1, f2 = torch.split(features, [bsz, bsz], dim=0)
         features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1)
-        print(f"features: {features.size()}")
-        print(f"labels: {labels.size()}")
+        # print(f"features: {features.size()}")
+        # print(f"labels: {labels.size()}")
         if opt.method == 'SupCon':
             loss = criterion(features, labels)
         elif opt.method == 'SimCLR':
