c01cdecd27c7ad99199871e8faebfb0b5365de03
Viet Dung <haconga1903@gmail.com>
2023-08-20 16:22:18 +0700

    Update main_supcon.py

--- main_supcon.py
+++ main_supcon.py
@@ -182,10 +182,10 @@ def set_loader(opt):
 
 def set_model(opt):
     # model = SupConResNet(name=opt.model)
-    set-head = False
+    set_head = False
     if(opt.use_head == 'True'):
-        set-head = True
-    model = pyramid_trans_expr2(img_size=224, num_classes=7, use_head = set-head)
+        set_head = True
+    model = pyramid_trans_expr2(img_size=224, num_classes=7, use_head = set_head)
     criterion = SupConLoss(temperature=opt.temp)
 
     # enable synchronized Batch Normalization
