7400a61a6d42aae36936f1710c2e8615a5b5c151
Viet Dung <haconga1903@gmail.com>
2023-09-05 11:27:16 +0700

    Update main_linear.py

--- main_linear.py
+++ main_linear.py
@@ -125,6 +125,7 @@ def set_model(opt):
         #         k = k.replace("module.", "")
         #         new_state_dict[k] = v
         #     state_dict = new_state_dict
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model = nn.DataParallel(model, device_ids=[0, 1])
         model.to(device)
         model = model.cuda()
